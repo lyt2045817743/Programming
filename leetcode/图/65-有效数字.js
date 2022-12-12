ea7a591dda03315475eb90779d0b074c6d2d8434
刘玉婷 <liuyuting@DESKTOP-N6C9QTE.mioffice.cn>
2022-12-12 17:22:34 +0800

    fix: 64-有效数字

--- leetcode/图/65-有效数字.js
+++ leetcode/图/65-有效数字.js
@@ -1,28 +1,33 @@
-var isNumber = function (s) {
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isNumber = function (s) {
   const graph = {
     0: { sign: 1, ".": 2, dight: 6 },
     1: { ".": 2, dight: 6 },
     2: { dight: 3 },
-    3: { dight: 3, e: 4 },
+    3: { dight: 3, e: 4, E: 4 },
     4: { dight: 5, sign: 7 },
     5: { dight: 5 },
-    6: { ".": 3, e: 4, dight: 6 },
+    6: { ".": 3, e: 4, E: 4, dight: 6 },
     7: { dight: 5 },
   };
   let position = 0;
   for (c of s.trim()) {
-    if (!isNaN(Number(c))) {
-      position = graph[position]["dight"];
+    if (c >= '0' && c <= '9') {
+    //   position = graph[position]["dight"];
+        c = 'dight';
     } else if (c === "+" || c === "-") {
-      position = graph[position]["sign"];
-    } else {
-      position = graph[position][c];
-    }
+    //   position = graph[position]["sign"];
+        c = 'sign';
+    } 
+    position = graph[position][c];
     if (!position) {
       return false;
     }
   }
-  return [3, 5, 6].includes(position);
+  return position === 3 || position === 5 || position === 6;
 };
 
 // test part
