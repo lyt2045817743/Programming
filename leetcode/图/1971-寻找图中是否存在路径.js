80590d36caa860b4495d933516519b52b45db013
liuyuting7 <liuyuting7@xiaomi.com>
2023-02-11 09:50:09 +0800

    EASY: 1971-寻找图中是否存在路径

--- leetcode/图/1971-寻找图中是否存在路径.js
+++ leetcode/图/1971-寻找图中是否存在路径.js
@@ -1,22 +1,25 @@
 var validPath = function(n, edges, source, destination) {
   const graph = {};
   let flag = false;
-  const hasVisited = [];
-  const nextVisitQueue = [source];
+  const hasVisited = new Array(n).fill(false);
+  let nextVisitPoints = [source];
   for (let i = 0; i < edges.length; i++) {
     addRoute(graph, edges[i][0], edges[i][1]);
     addRoute(graph, edges[i][1], edges[i][0]);
   }
+  hasVisited[source] = true;
 
-  console.log(graph);
-  while(nextVisitQueue.length) {
-    const curPoint = nextVisitQueue[0];
-    nextVisitQueue.shift();
+  while(nextVisitPoints.length) {
+    const from = nextVisitPoints[0];
+    nextVisitPoints.shift();
 
-    if (!graph[curPoint]) return false;
-    for (let i = 0; i < graph[curPoint].length; i++) {
-      
-    }
+    if (from === destination) return true;
+    graph[from]?.forEach(point => {
+      if(!hasVisited[point]) {
+        nextVisitPoints.push(point);
+        hasVisited[point] = true;
+      }
+    })
   }
 
   return flag;
@@ -32,5 +35,6 @@ var addRoute = function(graph, start, end) {
 }
 
 // const result = validPath(3, [[0,1],[1,2],[2,0]], 0, 2); // true
-const result = validPath(6, [[0,1],[0,2],[3,5],[5,4],[4,3]], 0, 5); // false
+// const result = validPath(6, [[0,1],[0,2],[3,5],[5,4],[4,3]], 0, 5); // false
+const result = validPath(1, [], 0, 0) // true
 console.log(result);
